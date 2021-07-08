382a4868b3f3d644bdb88bb75ba215089f60c3c9
Max Kayander <maxim.kayander1@gmail.com>
2021-07-08 22:52:10 +0300

    Implemented a working 'maxDepth' algorithm with recursion.

--- script.js
+++ script.js
@@ -5,9 +5,14 @@ console.log(root);
 /**
  * @param {HTMLElement} element
  * @param {number} level
+ * @returns {number}
  */
 const maxDepth = (element, level) => {
-    console.log(element.childElementCount);
+    if (element.childElementCount === 0) return level;
+
+    return Math.max(
+        ...Array.from(element.children).map((el) => maxDepth(el, level + 1))
+    );
 };
 
-maxDepth(root, {});
+console.log(maxDepth(root, 0));
