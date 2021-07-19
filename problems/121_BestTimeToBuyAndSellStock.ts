df4bfea12d85f549bdbb8e7a65c4fb37dea290a1
Max Kayander <maxim.kayander1@gmail.com>
2021-07-19 10:44:55 +0300

    Working leetcode solution

--- problems/121_BestTimeToBuyAndSellStock.ts
+++ problems/121_BestTimeToBuyAndSellStock.ts
@@ -1,43 +1,17 @@
 const maxProfit = (prices: number[]): number => {
-    // const minPrice = Math.min(...prices);
-    // const minPriceIndex = prices.indexOf(minPrice);
+    let minPrice = Infinity;
+    let result = 0;
 
-    // const maxAllowedPrice = Math.max(...prices.slice(minPriceIndex));
-
-    // return maxAllowedPrice - minPrice;
-
-    console.log("----", "\nProcessing prices: ", prices, "\n----");
-
-    const dealProfits = [];
-    for (const price of [...prices].sort()) {
-        const index = prices.indexOf(price);
-
-        if (index === prices.length - 1) {
-            console.log(
-                "skipping last price in the array - ",
-                price,
-                " at index ",
-                index
-            );
-            continue;
+    for (const [index, price] of prices.entries()) {
+        if (price < minPrice && index !== prices.length - 1) {
+            minPrice = price;
+            const currentResult = Math.max(...prices.slice(index + 1)) - price;
+            if (currentResult > result) result = currentResult;
         }
-
-        const maxPrice = Math.max(...prices.slice(index + 1));
-
-        console.log(
-            `New deal from price ${price} ; index: ${index} ; profit: ${
-                maxPrice - price
-            } ; maxPrice: ${maxPrice} ; maxPriceIndex: ${prices.indexOf(
-                maxPrice
-            )}`
-        );
-        dealProfits.push(maxPrice - price);
     }
 
-    console.log("dealProfits: ", dealProfits);
-
-    return Math.max(...dealProfits);
+    return result;
 };
 
-// console.log(maxProfit([7, 1, 5, 3, 6, 4]));
+console.log(maxProfit([7, 1, 5, 3, 6, 4]));
 console.log(maxProfit([2, 4, 1]));
