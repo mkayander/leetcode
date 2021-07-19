0e275603e819d78d72bcbf2963b6dc54a71e2655
Max Kayander <maxim.kayander1@gmail.com>
2021-07-19 10:51:20 +0300

    Optimized solution

--- problems/121_BestTimeToBuyAndSellStock.ts
+++ problems/121_BestTimeToBuyAndSellStock.ts
@@ -2,11 +2,13 @@ const maxProfit = (prices: number[]): number => {
     let minPrice = Infinity;
     let result = 0;
 
-    for (const [index, price] of prices.entries()) {
-        if (price < minPrice && index !== prices.length - 1) {
+    for (const price of prices) {
+        if (price < minPrice) {
             minPrice = price;
-            const currentResult = Math.max(...prices.slice(index + 1)) - price;
-            if (currentResult > result) result = currentResult;
+        }
+
+        if (price - minPrice > result) {
+            result = price - minPrice;
         }
     }
 
