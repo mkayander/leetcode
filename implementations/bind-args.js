4d7735dcf85276f1dd7a3eee73bc29e916dcfe15
Max Kayander <maxim.kayander1@gmail.com>
2021-09-03 11:16:06 +0300

    Added object method this argument binding example.

--- implementations/bind-args.js
+++ implementations/bind-args.js
@@ -6,3 +6,17 @@ const fx = ex.bind(null, 10);
 
 ex(12, 7);
 fx(7, 12);
+
+const obj = {
+    a: 42,
+    say: function () {
+        setTimeout(
+            function () {
+                console.log(this.a);
+            }.bind(this),
+            1000
+        );
+    },
+};
+
+obj.say();
