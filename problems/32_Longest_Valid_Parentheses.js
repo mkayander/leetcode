d745a9c5764c1b6cbbc1e5e0cbd0b0032cdb84ad
mkayander <maxim.kayander1@gmail.com>
2022-05-24 22:41:00 +0300

    Renamed arg

--- problems/32_Longest_Valid_Parentheses.js
+++ problems/32_Longest_Valid_Parentheses.js
@@ -1,15 +1,15 @@
 /**
  * {@link https://leetcode.com/problems/longest-valid-parentheses/discuss/1140004/JS-Python-Java-C%2B%2B-or-Easy-Stack-Solution-w-Explanation}
  *
- * @param {string} S
+ * @param {string} s
  * @return {number}
  */
-const longestValidParentheses = (S) => {
+const longestValidParentheses = (s) => {
     let stack = [-1];
     let ans = 0;
 
-    for (let i = 0; i < S.length; i++) {
-        if (S[i] === "(") {
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] === "(") {
             stack.push(i);
         } else if (stack.length === 1) {
             stack[0] = i;
