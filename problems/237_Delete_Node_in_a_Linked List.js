02b7fd8aebb042607c9f0d0cfbdfecbc644dfabf
mkayander <maxim.kayander1@gmail.com>
2022-10-13 20:43:04 +0200

    Added 237

---
+++ problems/237_Delete_Node_in_a_Linked List.js
@@ -0,0 +1,17 @@
+/**
+ * @typedef {{val: number, next: ListNode}} ListNode
+ *
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} node
+ * @return {void} Do not return anything, modify node in-place instead.
+ */
+const deleteNode = (node) => {
+    node.val = node.next.val;
+    node.next = node.next.next;
+};
