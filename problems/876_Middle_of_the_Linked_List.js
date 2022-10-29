cc221104ad97fe0dd6c29becaeb42006176ec718
mkayander <maxim.kayander1@gmail.com>
2022-10-29 18:09:46 +0200

    Added 876

---
+++ problems/876_Middle_of_the_Linked_List.js
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ *
+ * Time: O(n)
+ * Space: O(1)
+ */
+const middleNode = (head) => {
+    let left = head;
+    let right = head;
+
+    while (right && right.next) {
+        left = left.next;
+        right = right.next.next;
+    }
+
+    return left;
+};
